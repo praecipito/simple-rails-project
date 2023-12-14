191aadb03c6e0a4623b954d298d51e73f79895cf
Caio Alves <caioalvesfig@gmail.com>
2023-12-14 14:49:59 -0300

    task: index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :tasks
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "tasks#index"
 end
